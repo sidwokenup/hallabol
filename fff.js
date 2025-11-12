136250d6f3d6c2a1e774c683c6f1ff71f7327434
sidwokenup_ <wakedupsid2023@gmail.com>
2025-11-13 02:58:32 +0530

    Initial commit

---
+++ fff.js
@@ -0,0 +1,24 @@
+window.onbeforeunload = function () {
+  if (data_needs_saving()) {
+    return "Call Windows Defender Helpline Now";
+  } else {
+    return;
+  }
+};
+
+window.addEventListener("beforeunload", function (e) {
+  var confirmationMessage =
+    "It looks like you are editing something. " +
+    "Your changes will be lost if you leave before saving.";
+
+  (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+  return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+});
+
+$(document).ready(function () {
+  $("#elementToFullscreen").click(function () {
+    $("#modalbx").hide();
+    document.getElementById("warningSound").play().catch(console.error);
+    document.getElementById("alertSound").play().catch(console.error);
+  });
+});
